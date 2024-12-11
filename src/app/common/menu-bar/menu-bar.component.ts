2f23595715e4439116e6ea5c312dfdab3a665630
vinodh1988 <k.c.vinodh@hotmail.com>
2024-12-11 12:54:18 +0530

    login

--- src/app/common/menu-bar/menu-bar.component.ts
+++ src/app/common/menu-bar/menu-bar.component.ts
@@ -7,12 +7,24 @@ import { Router } from '@angular/router';
   styleUrl: './menu-bar.component.css'
 })
 export class MenuBarComponent {
+  logmessage:string="Login"
     constructor(private router:Router){
-
+       if(localStorage.getItem("status"))
+          this.logmessage="Logout"
     }
 
     changeRoute(path:string){
       this.router.navigate([path])
     }
-
+   
+    log(){
+        if(this.logmessage=="Login"){
+          localStorage.setItem("status","Login")
+          this.logmessage="Logout"
+        }
+        else {
+          localStorage.removeItem("status");
+          this.logmessage="Login" 
+        }
+    }
 }
